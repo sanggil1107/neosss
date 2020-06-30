738d27b45cfffdcc6f7b2b6ce04939ef6e9082ff
sanggil1107 <yangpang1107@gmail.com>
2020-07-01 00:04:19 +0900

    프로젝트 생성

---
+++ src/components/Toolbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Toolbar extends Component {
+    render() {
+        return(
+            <h1>툴바</h1>
+        );       
+    }
+}
+
+export default Toolbar;
