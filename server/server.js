8dfc6a286f8dc0d9a3d64c68e1aa4cee60c103bb
mink9321 <kimminji7595@naver.com>
2020-08-12 18:34:38 +0900

    server.js 수정

--- server/server.js
+++ server/server.js
@@ -4,13 +4,18 @@ const sql = require('mssql');
 const bodyParser = require('body-parser');
 //const api = require('./routes/index');
 const port =process.env.PORT || 3001;
+// const config = {
+//   user: 'neoss',
+//   password: 'new1234!',
+//   server: 'neoss.database.windows.net',
+//   database: 'NEOSSWORK'
+// };
 const config = {
-  user: 'neoss',
-  password: 'new1234!',
-  server: 'neoss.database.windows.net',
+  user: 'sa',
+  password: 'alswl@1021!',
+  server: 'DESKTOP-SOFF2LV',
   database: 'NEOSSWORK'
 };
-
 
 // app.use('/api', (req, res)=> res.json({username:'bryan'}));
 //app.use('/', api)
@@ -18,6 +23,30 @@ app.get('/api', (req, res) => {
   res.send({username:'home'});
 })
 
+app.get('/userList', (req, res) => {
+  sql.connect(config, function(err) {
+    if(err) {
+      console.log(err);
+    }
+
+    const request = new sql.Request();
+    request.input('TEAMCODE', 1);
+    request.output('USERID');
+    request.execute('UP_USERLIST_SELECT', function (err, rows) {
+      res.json(rows.recordset);
+      console.log(rows.recordset);
+      console.log(rows);
+    });
+    request.then(result => {
+      console.log(result)
+    });
+    // request.query('select * from tb_code', function (err, rows) {
+    //   res.json(rows.recordset);
+    //   console.log(rows.recordset);
+    //   console.log(rows);
+    // })
+  })
+});
 
 // app.get('/api', (req, res) => {
 //   res.json({username:'bryan'});
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class User extends Component {
-	render() {
-		return (
-			<h1>명단 관리</h1>
-		);
+	constructor(props) {
+	  super(props);
+	  this.state = {
+		userid: '',
+		username: '',
+		status: ''
+	  }
 	}
+  
+	componentDidMount() {
+	  this.callApi() 
+		.then(res => this.setState({userid: res.userid, username: res.username, status: res.status}))
+		.catch(err => console.log(err));
+	}
+  
+	callApi = async () => {
+	  const reponse = await fetch('/userList');
+	  const body = await reponse.json();
+	  console.log(reponse);
+	  return body;
+	}
+
+	render() {
+		return selectUserList();
+	}
+}
+
+// async function selectUserDB()
+// {
+// 	const { connPool } = require('./connect')
+
+// 	const pool = await connPool
+
+// 	await pool.request()
+// 	.input('TEAMCODE', 1)
+// 	.output('USERID')
+// 	.execute('UP_USERLIST_SELECT')
+// 	.then(result => {
+// 		console.log(result)
+// 	})
+// }
+
+
+
+function selectUserList()
+{
+	return (
+		<Fragment>
+			<head>사용자 명단 관리</head>
+			<body>
+				<h1>사용자 명단 관리</h1>
+				<div>
+					<table>
+						<thead>
+							<tr>
+								<td>아이디</td>
+								<td>이름</td>
+								<td>사용여부</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>{userid}</td>
+								<td>{username}</td>
+								<td>{status}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</body>
+		</Fragment>
+	);
 }
 
 export default User;
