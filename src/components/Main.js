8c33bc7b9112ff09059b527e5cbbafcd53eec62b
sanggil1107 <yangpang1107@gmail.com>
2020-10-26 23:58:12 +0900

    create admin

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import MenuBar from './Menubar';
 import Calendar  from './Calendar';
 import Register from './Register';
 import Settings from './Settings';
+import Admin from './Admin';
 import { BrowserRouter as Router, Switch ,Route } from 'react-router-dom';
 
 const Main = () => {
@@ -15,6 +16,7 @@ const Main = () => {
           <Route exact path="/main" component={Calendar}/>
           <Route exact path="/register" component={Register}/>
           <Route exact path="/settings" component={Settings}/>
+          <Route exact path="/admin" component={Admin}/>
         </Switch>
       </Router>
     </div>
