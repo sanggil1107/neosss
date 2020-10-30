1caadd8e5002b9b13f7d1852c68c40b65fb1125c
sanggil1107 <yangpang1107@gmail.com>
2020-10-30 17:29:02 +0900

    update setting

--- src/components/Setting_Check.js
+++ src/components/Setting_Check.js
@@ -5,10 +5,12 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { useFetch } from './Data';
 
 const Setting_Check = (props) => {
   const { open, setOpen } = props;
-  
+  const [ newAddFormState ] = useFetch();
+
   const handleclose = () => {
     setOpen(false);
   };
@@ -16,7 +18,7 @@ const Setting_Check = (props) => {
   return (
     <div>
       <Dialog open={open} onClose={handleclose} maxWidth="md">
-      <DialogTitle>알림팀 설정</DialogTitle>
+      <DialogTitle>알림팀 설정({newAddFormState})</DialogTitle>
           <DialogContent>
             <TextField label="이름" type="text" name="userName" ></TextField><br/>
             <TextField label="생년월일" type="text" name="birthday" ></TextField><br/>
