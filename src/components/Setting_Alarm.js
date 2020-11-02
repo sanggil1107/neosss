ee5b767d53fd05de7eacf28fae4c2d4d9723d1bc
sanggil1107 <yangpang1107@gmail.com>
2020-11-02 19:43:55 +0900

    update team_setting

--- src/components/Setting_Alarm.js
+++ src/components/Setting_Alarm.js
@@ -5,9 +5,11 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { useFetch } from './FetchTeam';
 
 const Setting_Alarm = (props) => {
   const { open, setOpen } = props;
+  const [ teams ] = useFetch();
   
   const handleclose = () => {
     setOpen(false);
@@ -18,13 +20,12 @@ const Setting_Alarm = (props) => {
       <Dialog open={open} onClose={handleclose} maxWidth="md">
       <DialogTitle>조근팀 설정</DialogTitle>
           <DialogContent>
-            <TextField label="이름" type="text" name="userName" ></TextField><br/>
-            <TextField label="생년월일" type="text" name="birthday" ></TextField><br/>
-            <TextField label="성별" type="text"name="gender" ></TextField><br/>
-            <TextField label="직업" type="text"name="job" ></TextField><br/>
+            { teams.map((team, i) => (
+              <p>{team.teamname}</p>
+            )) }
           </DialogContent>
           <DialogActions>
-            <Button varient="contained" color="primary" >추가</Button>
+            <Button varient="contained" color="primary" >확인</Button>
             <Button varient="outlined" color="primary" onClick={handleclose}>닫기</Button>
           </DialogActions>
       </Dialog>
--- src/components/Setting_Check.js
+++ src/components/Setting_Check.js
@@ -5,7 +5,6 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import axios from 'axios';
 import { useFetch } from './FetchTeam';
 
 const Setting_Check = (props) => {
@@ -29,13 +28,14 @@ const Setting_Check = (props) => {
   return (
     <div>
       <Dialog open={open} onClose={handleclose} maxWidth="md">
-      <DialogTitle>알림팀 설정()</DialogTitle>
+      <DialogTitle>알림팀 설정</DialogTitle>
           <DialogContent>
             { teams.map((team, i) => (
               <p>{team.teamname}</p>
             )) }
           </DialogContent>
           <DialogActions>
+            <Button varient="contained" color="primary" >확인</Button>
             <Button varient="outlined" color="primary" onClick={handleclose}>닫기</Button>
           </DialogActions>
       </Dialog>
