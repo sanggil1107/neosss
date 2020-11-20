3c2fb2671e66cadddffab5fbfcae59acd452ae01
sanggil1107 <yangpang1107@gmail.com>
2020-11-20 16:37:16 +0900

    소스정리

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules';
 import { BrowserRouter  } from 'react-router-dom';
 
-const store = createStore(rootReducer, composeWithDevTools());
-
 ReactDOM.render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
