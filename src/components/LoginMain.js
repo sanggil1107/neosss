5c0c4159375fc8c6813573eced00b91100443166
sanggil1107 <yangpang1107@gmail.com>
2020-11-21 22:42:57 +0900

    소스 정리

--- src/components/LoginMain.js
+++ src/components/LoginMain.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Login.css';
-
+  
 const LoginMain = (props) => {
   const [ userid, setUserid ] = useState();
   const [ user, setUser ] = useState([]);
